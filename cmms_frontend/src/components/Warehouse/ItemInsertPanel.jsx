54def9836b7ffbabbbf74c48a335e078f4d7170a
Tobiasz Ciesielski <t.ciesielski1177@gmail.com>
2021-01-13 10:16:37 +0100

    DataList to inputs implementation

--- cmms_frontend/src/components/Warehouse/ItemInsertPanel.jsx
+++ cmms_frontend/src/components/Warehouse/ItemInsertPanel.jsx
@@ -2,18 +2,51 @@ import React from 'react';
 import { Card, FormWrapper, InputForm } from '../../styleComponents';
 import ExitButton from './../common/ExitButton';
 import { Form } from './../../styleComponents';
+import { Input } from './../common/Input';
+import { useState } from 'react';
+
+
+const ListInput = ({placeholder, changeHandler, list, ...rest}) => {
+
+  const [itemId, setItemId] = useState(placeholder.toLowerCase().replace(' ', '_'))
+  return <>
+    <Input
+      name={itemId} // replace 'Item Name' with 'item_name'
+      placeholder={placeholder}
+      type="text"
+      changeHandler={changeHandler}
+      list={itemId} 
+      {...rest}
+    />
+    <datalist id={itemId}>
+      {list.map((item)=><option key={item} value={item} />)}
+    </datalist>
+  </>
+}
 
 const ItemInsertPanel = ({categories, isFetching}) => {
+
+  const list = ["test", "houston", "copy"]
+  const labels = [
+    "Item Name",
+    "Serial Number",
+    "Producer",
+    "Destiny",
+    "Description",
+    "Storing Location"
+  ]
+
+  const handleChange = ({ target }) => {
+    const { name, value } = target;
+    console.log(name, value)
+  };
+
   return <div className="container">
     <Card className="mt-4 mx-auto position-relative text-center">
-      <form action="submit">
-        <input className="form-control" style={{maxWidth: 200, marginTop: "5px"}} placeholder="Item name"/>
-        <input className="form-control" style={{maxWidth: 200, marginTop: "5px"}} placeholder="Serial number"/>
-        <input className="form-control" style={{maxWidth: 200, marginTop: "5px"}} placeholder="Category"/>
-        <input className="form-control" style={{maxWidth: 200, marginTop: "5px"}} placeholder="Producer"/>
-        <input className="form-control" style={{maxWidth: 200, marginTop: "5px"}} placeholder="Destiny"/>
-        <input className="form-control" style={{maxWidth: 200, marginTop: "5px"}} placeholder="Description"/>
-        <input className="form-control" style={{maxWidth: 200, marginTop: "5px"}} placeholder="Storing Location"/>
+      <ExitButton />
+      <form action="submit" onSubmit={(event)=>{event.preventDefault()}}>
+        {labels.map((label) => <ListInput style={{maxWidth: 200, marginTop: "5px"}} placeholder={label} list={list}/>)}
+        <button type="submit">Submit</button>
       </form>
     </Card>
   </div>
