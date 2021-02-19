bc2a0c4324f94751354687c5c7499197fdadacb2
Tobiasz Ciesielski <t.ciesielski1177@gmail.com>
2021-02-19 15:20:59 +0100

    Displaying items in warehouse dashboard

--- cmms_frontend/src/components/Warehouse/ItemList.jsx
+++ cmms_frontend/src/components/Warehouse/ItemList.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import styled from "styled-components";
 
-export const ItemCard = styled.div`
+const ItemCard = styled.div`
   background-color: whitesmoke;
   box-shadow: 3px 3px 8px rgb(0, 0, 0, 0.1);
   border-radius: 10px;
 `;
 
-export const Title = styled.div`
+const Title = styled.div`
   position: relative;
   display: flex;
   flex-direction: row;
@@ -21,7 +21,6 @@ export const Title = styled.div`
   .admin-buttons {
     align-self: center;
   }
-
 
   button {
     margin-left: 5px;
@@ -40,21 +39,87 @@ export const Title = styled.div`
   }
 `;
 
+const ItemImage = styled.img`
+  width: 180px;
+  height: 180px;
+  border-radius: 5px;
+  margin: 10px 20px 10px 30px;
+`
+
+const Properties = styled.div`
+  width:100%;
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  grid-gap: 10px;
+
+  ul {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    list-style: none;
+    margin: 15px 0;
+    padding: 0;
+  }
+
+  ul > li {
+    font-size: 16px;
+  }
+
+  ul > li > span {
+    font-size: 1.2em;
+    font-weight: 500;
+  }
+`
+
+const Body = styled.div`
+  display: flex;
+`
+
+function toBase64(arr) {
+   return btoa(
+      arr.reduce((data, byte) => data + String.fromCharCode(byte), '')
+   );
+}
+
 const ItemList = ({items}) => {
-  
-  return <> {items.map(i => <ItemCard key={i.itemId} className="mt-4">
+  return <> {items.map(item => <ItemCard key={item.itemId} className="mt-4">
+      {console.log(item.image)}
       <Title>
-        <h4>{i.itemName}</h4>
+        <h4>{item.itemName}</h4>
         <div className="admin-buttons">
           <button className="btn btn-primary">Edit</button>
           <button className="btn btn-danger">Delete</button>
-        </div>
+        </div> 
       </Title>
-      <ul>
-        <li>{i.Producer.producerName}</li>
-        <li>{i.SubSubCategory.subSubCategoryName}</li>
-        <li>{i.StoringLocation.StoringLocationName}</li>
-      </ul>
+      <Body>
+        <ItemImage src={!item.image 
+          ? require("../assets/image-not-found.svg") 
+          : `data:image/png;base64,${toBase64(item.image.data)}`}
+        />
+        <Properties>
+          <ul>
+            <li>Serial Number: <span>{item.serialNumber}</span></li>
+            <li>Producer Name: <span>{item.Producer.producerName}</span></li>
+            <li>Producer Code: <span>{item.Producer.producerCode}</span></li>
+            <li>Category: <span>{item.SubSubCategory.subSubCategoryName}</span></li>
+          </ul>
+          <ul>
+            <li>Description: <b>{item.description}</b></li>
+            <li>Destiny: <b>{item.destiny}</b></li>
+            <li>In Stock: <span>{item.inStock}</span></li>
+          </ul>
+          <ul className=" mr-2">
+            <li className="w-100"><button className="btn btn-secondary w-100">Technicalities</button></li>
+            <li>
+            <div className="d-flex justify-content-center"><div className="mb-2">You have rented: 0</div></div>
+              <div className="rental d-flex">
+                <button className="btn btn-outline-dark w-100 mr-1">Return</button>
+                <button className="btn btn-success w-100 ml-1">Rent</button>
+              </div>
+            </li>
+          </ul>
+        </Properties>
+      </Body>
       </ItemCard>
     )}
   </>
